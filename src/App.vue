c76a0757be77dd885c0b7173f1831226b7165d42
Lin-Ya <fenghuiwing@outlook.com>
2018-06-20 20:09:32 +0800

    todo: 封装md编辑器

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,5 @@ export default {
 </script>
 
 <style>
+@import "simplemde/dist/simplemde.min.css";
 </style>
---
+++ src/components/MarkdownEditor.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="markdown-editor">
+    <textarea></textarea>
+  </div>
+</template>
+
+<script>
+import simplemde from 'simplemde';
+import marked from 'marked'
+export default {
+  name: 'markdown-editor',
+  props: {
+    
+  },
+  data () {
+    return {
+
+    }
+  },
+  mounted(){
+    console.log('进入了md编辑器')
+    let editor = new simplemde({
+      element: this.$el.firstElementChild
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.markdown-editor .markdown-body {
+  padding: 0.5em
+}
+.markdown-editor .editor-preview-active, .markdown-editor .editor-preview-active-side {
+  display: block;
+}
+</style>

6b949a85f59cc787d5f062815ba566df7b5cb54b
Lin-Ya <fenghuiwing@outlook.com>
2018-07-13 10:08:57 +0800

    设计好了发布博文的基本流程处理

--- src/store/actions.js
+++ src/store/actions.js
@@ -34,40 +34,8 @@ const actions = {
     //缓存待上传的article
     commit(mutations.SET_TEMPPOSTARTICLE, article)
     this.dispatch('filterTags').then(obj => {
-      console.log(obj.newTagGroup)
-      console.log(obj.existTagGroup)
+      this.dispatch('setPostData', obj)
     })
-
-
-
-    //set article存储
-    // let Article = new AV.Object('Article')
-    // Article.set('title', article.title)
-    // Article.set('content', article.content)
-    // Article.set('cover', article.cover)
-    // Article.set('owner', AV.User.current())
-
-    //set tags存储
-
-
-    //set Post存储 使用中间表实现多对多关系
-    // let Post = new AV.Object('Post')
-    // Post.set('article', Article)
-    // Post.set('tags', TagsGroup)
-    // Post.set('abstract', article.abstract)
-    // Post.set('owner', AV.User.current())
-
-
-    // Post.save().then(
-    //   function (res) {
-    //     console.log('发布成功')
-    //     console.log(res)
-    //   },
-    //   function (error) {
-    //     console.log(JSON.stringify(error))
-    //   }
-    // )
-
   },
 
   getTagsList({ commit }) {
@@ -90,11 +58,11 @@ const actions = {
     });
   },
 
-  filterTags({ commit }) {
+  filterTags({ commit, state }) {
     return new Promise((resolve, reject) => {
       let newTagGroup = [], existTagGroup = []  //new存放新的tag，exist存放已有的tag，后面用于绑定
-      let tagsList = this.state.tag.tagsList
-      let article = this.state.article
+      let tagsList = state.tag.tagsList
+      let article = state.article
       //对标签进行检索，如果是旧标签，就把旧标签的id存放到existTagGroup;如果是新标签，则把tag存放到newTagGroup
       article.tags.forEach(element => {
         console.log(element)
@@ -103,12 +71,59 @@ const actions = {
         })
         exist ? existTagGroup.push(exist.id) : newTagGroup.push(element)
       });
-
       resolve({ newTagGroup, existTagGroup })
     })
   },
-  uploadPost({ commit }) {
 
+  setPostData({ commit, state }, obj) {
+    let article = state.article
+
+    // set article存储
+    let Article = new AV.Object('Article')
+    Article.set('title', article.title)
+    Article.set('content', article.content)
+    Article.set('cover', article.cover)
+    Article.set('owner', AV.User.current())
+
+    //set 新Tag存储
+    let TagsGroup = []
+    if (obj.newTagGroup.length > 0) {
+      obj.newTagGroup.forEach(newTag => {
+        let Tags = new AV.Object('Tags')
+        Tags.set('tag', newTag)
+        TagsGroup.push(Tags)
+      })
+    }
+    if (obj.existTagGroup.length > 0) {
+      //oldTag存储的是这个旧tag的objectId
+      obj.existTagGroup.forEach(oldTag => {
+        let Tags = AV.Object.createWithoutData('Tags', oldTag)
+        TagsGroup.push(Tags)
+      })
+    }
+    console.log('TagsGroup')
+    console.log(TagsGroup)
+
+    //set Post存储 使用中间表实现多对多关系
+    let Post = new AV.Object('Post')
+    Post.set('article', Article)
+    Post.set('tags', TagsGroup)
+    Post.set('abstract', article.abstract)
+    Post.set('owner', AV.User.current())
+
+    this.dispatch('uploadPost',Post)
+  },
+
+  uploadPost({ commit }, postData) {
+    postData.save().then(
+      function (res) {
+        console.log('发布成功')
+        console.log(res)
+      },
+      function (error) {
+        console.log(JSON.stringify(error))
+      }
+    )
   }
 
 }

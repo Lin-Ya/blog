9e633b3fa05690a614c2cbff583b7239727bfcb5
Lin-Ya <fenghuiwing@outlook.com>
2018-07-24 16:42:40 +0800

    初步完成导航栏样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="home">
-    <div class="slide">
-      <Side class="side-wrapper" />
-    </div>
+    <Side class="side-wrapper" />
     <div class="main-wrapper">
       <Nav class="nav-wrapper" />
       <Posts class="posts-wrapper" />
@@ -21,15 +19,20 @@ export default {
     Nav,
     Side
   },
-  data(){
-    return {
-      
-    }
+  data() {
+    return {};
   }
 };
 </script>
 
 <style lang="less">
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
 //home layout
 .home {
   height: 100%;
@@ -37,28 +40,30 @@ export default {
   display: flex;
   flex-direction: row;
   align-items: center;
-  .slide {
+  .side-wrapper {
     width: 88px;
     height: 100%;
-    .side-wrapper {
-      height: 100%;
-    }
+  }
+  .main-wrapper {
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+  }
+}
+@media (max-width: 768px) {
+  .side-wrapper {
+    display: none;
   }
   .main-wrapper {
     flex: 1;
     height: 100%;
     .nav-wrapper {
-      height: 104px;
+      min-height: 56px;
+      margin-bottom: 8px;
     }
     .posts-wrapper {
-      height: calc(100% - 104px);
-      overflow: scroll;
+      height: calc(100vh - 56px);
     }
-  }
-}
-@media (max-width: 768px){
-  .slide {
-    display: none;
   }
 }
 </style>
--- src/views/components/Nav.vue
+++ src/views/components/Nav.vue
@@ -2,33 +2,111 @@
   <div>
     <ul class="nav">
       <li>
-        <router-link to="/">Home</router-link>
+        <router-link to="/">HOME</router-link>
       </li>
       <li>
-        <router-link :to="{name:'Tags'}">Tags</router-link>
+        <router-link :to="{name:'Tags'}">TAGS</router-link>
       </li>
       <li>
-        <router-link :to="{name:'Mdeditor'}">Editor</router-link>
+        <router-link :to="{name:'Mdeditor'}">EDITOR</router-link>
       </li>
       <li>
-        <router-link :to="{name:'Account'}">Account</router-link>
+        <router-link :to="{name:'Account'}">LOGIN</router-link>
+      </li>
+    </ul>
+    <ul class="nav-mobile clearfix">
+      <li class="nav-menu">
+        <icon @click="showMenu">
+          <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-ego-menu"></use>
+          </svg>
+        </icon>
+        <ul class="clearfix" v-if="this.menuStatus">
+          <li>
+            <router-link :to="{name:'Tags'}">TAGS</router-link>
+          </li>
+          <li>
+            <router-link :to="{name:'Mdeditor'}">EDITOR</router-link>
+          </li>
+          <li>
+            <router-link :to="{name:'Account'}">LOGIN</router-link>
+          </li>
+        </ul>
+      </li>
+      <li class="nav-home">
+        <router-link to="/">HOME</router-link>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      menuStatus: false
+    };
+  },
+  methods: {
+    showMenu() {
+      this.menuStatus = !this.menuStatus;
+    }
+  }
+};
 </script>
 
 <style lang="less">
-.nav {
-  padding: 24px;
-  display: flex;
-  align-items: center;
-  li {
-    font-size: .44rem;
-    padding: 8px 10px;
+
+@media (min-width: 769px) {
+  .nav {
+    padding: 24px;
+    height: 100%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    font-size: 24px;
+    line-height: 24px;
+    li {
+      margin: 0 8px;
+    }
+  }
+  .nav-mobile {
+    display: none;
+  }
+}
+@media (max-width: 768px) {
+  .nav {
+    display: none;
+  }
+  .nav-mobile {
+    position: relative;
+    text-align: center;
+    padding: 0.1481rem;
+    & > li {
+      font-size: 0.5926rem;
+      line-height: 0.5926rem;
+      padding: 0.1481rem;
+      width: auto;
+    }
+    .nav-menu {
+      float: left;
+      width: 2.7778rem;
+      ul {
+        display: flex;
+        flex-direction: column;
+        li {
+          padding: 0.1481rem;
+          width: 100%;
+  
+        }
+      }
+    }
+    .nav-home {
+      font-weight: 600;
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+    }
   }
 }
 </style>
